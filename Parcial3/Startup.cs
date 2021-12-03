2b046355e6e8492c10b34753130d42e0274ed212
EdGh1123 <eduardodelahozguerrero@gmail.com>
2021-12-02 22:32:34 -0500

    Initial commit

---
+++ Parcial3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Parcial3.Startup))]
+namespace Parcial3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
